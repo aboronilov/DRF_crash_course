2918e8c02aa9b8b8fdb2efa43a3b9bd51bd02874
= <=>
2022-06-10 09:30:29 +0200

    DRF API view

--- api/views.py
+++ api/views.py
@@ -3,14 +3,20 @@ import json
 from django.forms import model_to_dict
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 from products.models import Product
 
 
+@api_view(http_method_names=['GET'])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data)
-    return JsonResponse(data)
+    return Response(data)
