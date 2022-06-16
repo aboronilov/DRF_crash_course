630ce7cc223b6901cced7b39a5cc534cdadd05b1
= <=>
2022-06-16 10:28:57 +0200

    destroy and update API view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProductDetailAPIView, ProductListCreateAPIView, product_alt_view
+from .views import ProductDetailAPIView, ProductListCreateAPIView, ProductUpdateAPIView, ProductDeleteAPIView
 
 urlpatterns = [
     path('<int:pk>/', ProductDetailAPIView.as_view()),
+    path('<int:pk>/update/', ProductUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', ProductDeleteAPIView.as_view()),
     path('', ProductListCreateAPIView.as_view())
 ]
