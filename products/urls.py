9c335744cf78044751f38e9cdff5421d28fd18ff
= <=>
2022-06-20 08:28:50 +0200

    mixin views

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductDetailAPIView, ProductListCreateAPIView, ProductUpdateAPIView, ProductDeleteAPIView
+from .views import ProductDetailAPIView, ProductListCreateAPIView, ProductUpdateAPIView, ProductDeleteAPIView, \
+    ProductMixinView
 
 urlpatterns = [
-    path('<int:pk>/', ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', ProductUpdateAPIView.as_view()),
-    path('<int:pk>/delete/', ProductDeleteAPIView.as_view()),
-    path('', ProductListCreateAPIView.as_view())
+    path('<int:pk>/', ProductMixinView.as_view()),
+    path('<int:pk>/update/', ProductMixinView.as_view()),
+    path('<int:pk>/delete/', ProductMixinView.as_view()),
+    path('', ProductMixinView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -50,6 +50,33 @@ class ProductListAPIView(generics.ListAPIView):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 @api_view(http_method_names=['GET', 'POST'])
