f16d39997ea23d0ac7926f26f7ef0e607020eaac
= <=>
2022-06-27 11:37:08 +0200

    foreign key serialization

--- products/serializers.py
+++ products/serializers.py
@@ -1,30 +1,43 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
 from .models import Product
 from .validators import validate_title_no_hello, unique_validator
 
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(read_only=True, source='user')
+    related_products = ProductInlineSerializer(read_only=True, source='user.product_set.all', many=True)
     details = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail")
     title = serializers.CharField(validators=[validate_title_no_hello, unique_validator])
-    # name = serializers.CharField(read_only=True, source='title')
+    # email = serializers.EmailField(source='user.email', read_only=True)
 
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'owner',
             'url',
             'edit_url',
-            # 'name',
+            # 'email',
             'pk',
             'title',
             'content',
             'price',
             'sale_price',
-            'details'
+            'details',
+            'related_products'
         ]
 
     # можно делать отдельно serializers под каждый метод crud
