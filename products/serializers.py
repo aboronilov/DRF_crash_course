bac3596312497b16c5b209ec54bc20f72ac4acf9
= <=>
2022-06-27 12:06:43 +0200

    pagination

--- products/serializers.py
+++ products/serializers.py
@@ -17,12 +17,9 @@ class ProductInlineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(read_only=True, source='user')
-    related_products = ProductInlineSerializer(read_only=True, source='user.product_set.all', many=True)
-    details = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail")
     title = serializers.CharField(validators=[validate_title_no_hello, unique_validator])
-    # email = serializers.EmailField(source='user.email', read_only=True)
 
     class Meta:
         model = Product
@@ -30,32 +27,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'owner',
             'url',
             'edit_url',
-            # 'email',
             'pk',
             'title',
             'content',
             'price',
             'sale_price',
-            'details',
-            'related_products'
         ]
 
     # можно делать отдельно serializers под каждый метод crud
-    # def create(self, validated_data):
-    #     email = validated_data.pop('email')
-    #     super().create(validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     super().update(instance, validated_data)
-
-    # def validate_title(self, value):
-    #     # iexact - без учета регистра
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     qs = Product.objects.filter(user=user, title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name")
-    #     return value
 
     def get_edit_url(self, obj):
         request = self.context.get('request')
@@ -63,9 +42,3 @@ class ProductSerializer(serializers.ModelSerializer):
             return None
         return reverse(viewname="product-edit", kwargs={'pk': obj.pk}, request=request)
 
-    def get_details(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_random_info()
