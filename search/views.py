a1bfca3b6c4fb47150a088cd3ba8f9289aa6ea26
= <=>
2022-06-29 10:31:20 +0200

    agolia client search

--- search/views.py
+++ search/views.py
@@ -1,10 +1,26 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from products.models import Product
 from products.serializers import ProductSerializer
+from .client import perform_search
 
 
-class SearchListView(generics.ListAPIView):
+class SearchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        user = None
+        if request.user.is_authenticated:
+            user = request.user.username
+        query = request.GET.get('q')
+        public = str(request.GET.get('public')) != '0'
+        tags = request.GET.get('tag') or []
+        if not query:
+            return Response('', status=404)
+        results = perform_search(query=query, tags=[tags], user=user, public=public)
+        return Response(results)
+
+
+class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
